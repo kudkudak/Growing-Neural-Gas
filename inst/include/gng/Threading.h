f338511b61a1d7e591d65d78e432ef02cc64cf21
kudkudak <staszek.jastrzebski@gmail.com>
2014-06-09 08:37:36 +0200

    Reorganized files, new Makevars

---
+++ inst/include/gng/Threading.h
@@ -0,0 +1,24 @@
+/*
+ * File:   threading_wrapper.h
+ * Author: staszek
+ *
+ * Created on June 6, 2014, 6:37 AM
+ *
+ * Contains code wrapping threading independently of platform. Currently using tinythread
+ */
+
+#ifndef THREADING_H
+#define THREADING_H
+
+#include "tinythread/tinythread.h"
+
+namespace gmum{
+	///recursive mutex providing interface as boost::recursive_mutex
+	typedef tthread::recursive_mutex gmum_recursive_mutex;
+	typedef tthread::thread gmum_thread ;
+	typedef tthread::condition_variable gmum_condition;
+	///sleep current thread (equivalent to boost::this_thread::sleep)
+	void sleep(int ms);
+}
+
+#endif
---
+++ src/gng/Threading.cpp
@@ -0,0 +1,9 @@
+#include "Threading.h"
+
+namespace gmum{
+
+	void sleep(int ms){
+		tthread::this_thread::sleep_for(tthread::chrono::milliseconds(ms));
+	}
+
+}
