85a5e1578569cb21838acdc6286e24aca26b5bec
Nominalista <hatalski.marcin@gmail.com>
2014-12-13 20:08:25 +0100

    Next renaming

--- src/gng/GNGServer.cpp
+++ src/gng/GNGServer.cpp
@@ -67,9 +67,7 @@ void GNGServer::init(GNGConfiguration configuration, std::istream * input_graph)
 		cerr << GNGConfiguration::DatasetSampling << endl;
 		cerr << GNGConfiguration::DatasetSamplingProb << endl;
 		DBG(m_logger,11, "GNGServer::Not recognized dataset");
-		throw BasicException(
-				"Database type not supported "
-						+ to_string(current_configuration.datasetType));
+		throw BasicException("Database type not supported " + toString(current_configuration.datasetType));
 	}
 
 	DBG(m_logger,10, "GNGServer()::gngDatabase constructed");
