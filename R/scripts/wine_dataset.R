73732df3c4269fa80eeac416f2731ca32c0c7c68
kudkudak <staszek.jastrzebski@gmail.com>
2015-01-03 17:01:33 +0100

    Added prediction testing to wine_dataset.R

--- R/scripts/wine_dataset.R
+++ R/scripts/wine_dataset.R
@@ -3,7 +3,7 @@ data(wine, package="rattle")
 scaled.wine <- scale(wine[-1])
 
 # Train in an offline manner
-gng <- GNG(scaled.wine, labels=wine$Type, max.nodes=70, 
+gng <- GNG(scaled.wine, labels=wine$Type, max.nodes=20, 
            training=gng.train.offline(max.iter=10000, min.improvement=1e-1))
 
 
@@ -20,3 +20,11 @@ plot(gng, mode = gng.plot.2d,
 
 # Print summary of trained object
 print(summary(gng))
+
+# Print prediction accuracy
+labels = as.vector(wine[,c("Type")], mode="double") 
+preds <- c()
+for(i in 1:nrow(scaled.wine)){
+  preds <- c(preds,round(node(gng,  round(predict(gng, as.vector(scaled.wine[i,], mode="double")))+1)$label))
+}
+print(table(preds, labels))
